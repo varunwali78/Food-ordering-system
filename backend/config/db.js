eee5ade1099fe724c087cb9cfc37b4065435e32e
varunwali <varunwali36@gmail.com>
2025-01-14 19:50:30 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://varunwali36:BClyDeCPZGPfpYNx@cluster0.smiahg0.mongodb.net/food-del"
-    )
-    .then(() => {
-      console.log("DB connected");
-    });
+  await mongoose.connect("mongodb+srv://").then(() => {
+    console.log("DB connected");
+  });
 };
